f59b6e290df9f2e4eac366251c929e75a722b206
Vi Minh Tien <20520810@gm.uit.edu.vn>
2022-07-28 01:56:55 +0700

    config/app BackupServiceProvider

--- config/app.php
+++ config/app.php
@@ -175,6 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Spatie/laravel-backup
+        Spatie\Backup\BackupServiceProvider::class,
+
     ],
 
     /*
